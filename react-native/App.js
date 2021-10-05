84cf97e1451edab5e6cd3c99f29623dabc20e47e
erictelkkala <52538447+erictelkkala@users.noreply.github.com>
2021-10-05 21:40:10 +0300

    Home page cosmetic fixes

--- react-native/App.js
+++ react-native/App.js
@@ -1,57 +1,50 @@
-import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Button, IconButton, Colors } from "react-native-paper";
-import StartSessionsScreen from "./components/Session";
-import BeginSession from "./components/BeginSession";
-import ViewSessionsScreen from "./components/ViewSessions";
+import * as React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Button, IconButton, Colors } from 'react-native-paper'
+import StartSessionsScreen from './components/Session'
+import BeginSession from './components/BeginSession'
+import ViewSessionsScreen from './components/ViewSessions'
 
 function HomeScreen({ navigation }) {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 50,
-      }}
-    >
-	<View style={styles.container}>
-      	<Text style={styles.vechicleCounter}>
-		  VECHICLE COUNTER
-		</Text>
-    
-		<IconButton
-    		icon="car-multiple"
-    		color={Colors.black}
-    		size={50}
-			style={styles.vechicleIcon}
-  			/>
-	</View>
-      <Button
-        icon="arrow-right-circle"
-        style={{ width: 175, height: 60 }}
-        contentStyle={{ marginTop: 10 }}
-        mode="contained"
-        onPress={() => navigation.navigate("Start Session")}
-      >
-        Start session
-      </Button>
-      <Button
-        style={{ marginTop: 20, width: 220, height: 60 }}
-        contentStyle={{ marginTop: 10 }}
-        icon="border-color"
-        mode="contained"
-        onPress={() => navigation.navigate("View Sessions")}
-      >
-        Previous sessions
-      </Button>
+    <View style={styles.main}>
+      <View style={styles.container}>
+        <Text style={styles.vechicleCounter}>VEHICLE COUNTER</Text>
+
+        <IconButton
+          icon="car-multiple"
+          color={Colors.black}
+          size={50}
+          style={styles.vechicleIcon}
+        />
+      </View>
+      <View style={styles.buttonColumn}>
+        <Button
+          icon="arrow-right-circle"
+          style={{ width: 175 }}
+          contentStyle={{ height: 60 }}
+          mode="contained"
+          onPress={() => navigation.navigate('Start Session')}
+        >
+          Start session
+        </Button>
+        <Button
+          style={{ marginTop: 20 }}
+          contentStyle={{ height: 60 }}
+          icon="border-color"
+          mode="contained"
+          onPress={() => navigation.navigate('View Sessions')}
+        >
+          Previous sessions
+        </Button>
+      </View>
     </View>
-  );
+  )
 }
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 function App() {
   return (
@@ -63,28 +56,40 @@ function App() {
         <Stack.Screen name="Begin Session" component={BeginSession} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 const styles = StyleSheet.create({
-	container: {
-	  width: 208,
-	  height: 74
-	},
-	vechicleCounter: {
-	  fontFamily: "sans-serif",
-	  fontStyle: "italic",
-	  fontWeight: "bold",
-	  color: "black",
-	  fontSize: 41,
-	  textAlign: "center",
-	  marginVertical: -190,
-	  width: 208,
-	  height: 94
-	},
-	vechicleIcon: {
-		alignSelf: "center",
-		marginVertical: 200
-	  }
-  });
+  main: {
+    width: '100%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+  },
+  container: {
+    height: '20%',
+    paddingBottom: 100,
+  },
+  vechicleCounter: {
+    fontFamily: 'sans-serif',
+    position: 'relative',
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 41,
+    textAlign: 'center',
+    paddingBottom: 0,
+    width: 208,
+    height: 94,
+  },
+  vechicleIcon: {
+    alignSelf: 'center',
+    position: 'relative',
+  },
+  buttonColumn: {
+    marginTop: 200,
+    alignItems: 'center',
+  },
+})
 
-export default App;
+export default App
