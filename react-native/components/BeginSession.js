ce1d994ac94085a5d26dff369dff83eac3eddc00
Sylphyyy <toxicmelon123@gmail.com>
2021-10-12 10:42:20 +0300

    Added SessionID generator (npm install)

--- react-native/components/BeginSession.js
+++ react-native/components/BeginSession.js
@@ -10,13 +10,14 @@ import { Avatar, Button, IconButton, useTheme } from 'react-native-paper'
 import { useNavigation } from '@react-navigation/native'
 import { addInfo, fetchAllInfo } from '../database/db'
 import Timer from 'react-compound-timer'
+import { v4 as uuidv4 } from 'uuid'
 
 // Main function
 
 export default function BeginSession() {
   //Some variables for time being.
-  let SessionID = 1
   let UserID = 1
+  var SessionID=uuidv4();
 
   // Theme import
   const theme = useTheme()
@@ -31,7 +32,6 @@ export default function BeginSession() {
   const [longitude, setLongitude] = useState(0.0)
   const [currentDate, setDate] = useState('')
   // const [UserID, setUserID] = useState(0)
-  //  const [SessionID, setSessionID] = useState(0)
 
   useEffect(() => {
     var date = new Date().getDate()
@@ -52,6 +52,7 @@ export default function BeginSession() {
         bus: setbusCount,
         trucks: settruckCount,
         motorcycles: setmopedCount,
+        SessionID: SessionID,
         date: setDate,
         longitude: setLongitude,
         latitude: setLatitude,
@@ -68,8 +69,8 @@ export default function BeginSession() {
         mopedCount,
         busCount,
         truckCount,
-        UserID,
         SessionID,
+        UserID,
         currentDate,
         longitude,
         latitude
