094d990b13a5f658679f06d38e794f1bd1d86ea7
Sylphyyy <toxicmelon123@gmail.com>
2021-10-12 14:07:30 +0300

    Added bar charts to previous sessions screen (npm install)

--- react-native/components/ViewSessions.js
+++ react-native/components/ViewSessions.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { View, Text, FlatList, Button, StyleSheet } from 'react-native'
+import { View, Text, FlatList, Button, StyleSheet, Dimensions } from 'react-native'
+import { BarChart } from "react-native-chart-kit";
 import { fetchAllInfo } from '../database/db'
 
 export default function ViewSessionsScreen() {
@@ -29,6 +30,27 @@ export default function ViewSessionsScreen() {
           data={readAllData}
           renderItem={(itemData) => (
             <View>
+              <BarChart style={styles.styleChart}
+                data={{
+                  labels: ['Car', 'Bus', 'Trucks', 'Motorcycles'],
+                  datasets: [{ data: [itemData.item.car, itemData.item.bus, itemData.item.trucks, itemData.item.motorcycles] }],
+                }}
+                width={Dimensions.get('window').width - 10}
+                height={200}
+                yAxisInterval={2}
+                segments={3}
+                showValuesOnTopOfBars={true}
+                chartConfig={{
+                  decimalPlaces: 0,
+                  backgroundColor: '#1cc910',
+                  backgroundGradientFrom: '#6E32D1',
+                  backgroundGradientTo: '#6E32D1',
+                  color: (opacity = 255) => 'white',
+                  style: {
+                    borderRadius: 12, padding: 10
+                  },
+                }}
+              />
               <Text style={styles.counters}>
                 Car : {itemData.item.car}
                 {'  '}
@@ -64,4 +86,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     padding: 20,
   },
+  styleChart: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
