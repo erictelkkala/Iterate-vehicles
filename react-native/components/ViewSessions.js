6360cbf8c50f0391e3fc46b38c08ad9fb1f7c0db
romatsypin <romatsypin@gmail.com>
2021-10-14 01:45:18 +0300

    Added a lot of viewing

--- react-native/components/ViewSessions.js
+++ react-native/components/ViewSessions.js
@@ -1,49 +1,12 @@
-import React, { useState } from 'react'
-import { View, Text, FlatList, StyleSheet, Dimensions } from 'react-native'
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
 import { Button } from 'react-native-paper'
 import { useNavigation } from '@react-navigation/native'
-import { BarChart } from 'react-native-chart-kit'
-import { fetchAllInfo } from '../database/db'
 
 export default function ViewSessionsScreen() {
   const navigation = useNavigation()
-  const [readAllData, setInfo] = useState([])
-  const [cloudData, setCloudData] = useState([])
   var index = 1
-
-  async function readAllDataToo() {
-    await fetchAllInfo()
-      .then((res) => {
-        setInfo(res.rows._array)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-      .finally(() => {
-        console.log('All fish are read')
-      })
-  }
-  async function fetchData() {
-    await fetch(
-      'https://monkesproject.appspot.com/rest/counterservice/getAll'
-    ).then((parameter) =>
-      parameter
-        .json()
-        .catch((err) => {
-          setSomeErrors(err)
-          setErrors(true)
-          console.log('JSON Error: ' + err)
-        })
-        .then((anotherParameter) => {
-          setCloudData(anotherParameter)
-          console.log(anotherParameter)
-        })
-        .catch((anError) => {
-          setSomeErrors(anError)
-          console.log(anError)
-        })
-    )
-  }
+  
   return (
     <View
       style={{
@@ -58,7 +21,7 @@ export default function ViewSessionsScreen() {
           mode="contained"
           style={styles.buttonStyles}
           contentStyle={{ height: 60 }}
-          onPress={readAllDataToo}
+          onPress={() => navigation.navigate('Local Session')}
         >
           Local Sessions
         </Button>
@@ -66,52 +29,10 @@ export default function ViewSessionsScreen() {
           mode="contained"
           style={styles.buttonStyles}
           contentStyle={{ height: 60 }}
-          onPress={fetchData}
+          onPress={() => navigation.navigate('Cloud Session')}
         >
           Cloud Sessions
         </Button>
-        <FlatList
-          // keyExtractor={item=>item.id.toString()}
-          keyExtractor={(item) => readAllData.indexOf(item).toString()}
-          data={readAllData}
-          renderItem={(itemData) => (
-            <View>
-              <Text style={styles.counters}>
-                Date: {itemData.item.Date} {itemData.item.Timer}
-              </Text>
-              <Text>Map goes here</Text>
-              <Button
-                mode="contained"
-                style={styles.buttonStyles}
-                contentStyle={{ height: 60 }}
-                onPress={() => navigation.navigate('View Single Session')}
-              >
-                Details
-              </Button>
-            </View>
-          )}
-        />
-        <FlatList
-          // keyExtractor={item=>item.id.toString()}
-          keyExtractor={(item) => cloudData.indexOf(item).toString()}
-          data={cloudData}
-          renderItem={(itemData) => (
-            <View>
-              <Text style={styles.counters}>
-                Date: {itemData.item.date} {itemData.item.timer}
-              </Text>
-              <Text>Map goes here</Text>
-              <Button
-                mode="contained"
-                style={styles.buttonStyles}
-                contentStyle={{ height: 60 }}
-                onPress={() => navigation.navigate('View Single Session')}
-              >
-                Details
-              </Button>
-            </View>
-          )}
-        />
       </View>
     </View>
   )
@@ -127,11 +48,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 20,
-  },
-  buttonStyles: {
-    width: 170,
-    alignSelf: 'center',
     marginTop: 20,
   },
   buttonStyles: {
